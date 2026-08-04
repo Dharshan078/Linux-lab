3a1270b2ba32539599122f4080b6bc7130c60445
Dharshan <sivadharshan980@gmail.com>
2026-08-04 11:05:54 +0000

    Health report Script v1.2

--- Scripts/health-report.sh
+++ Scripts/health-report.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-#======== Commands and Variable ========
+
+#################################################
+# Linux Server Health Report Script
+# Author: Sivadharshan
+# Version: 1.2
+#################################################
+
 ComputerName=$(hostname)
 CurrentUser=$(whoami)
 CurrentDate=$(date)
 KernelName=$(uname -r)
-CPUUsage=$(top -bn 1 | head -5)
-RAMUsage=$(free -h)
-DIskUsage=$(df -h)
-Top5Processes=$(ps aux --sort=-%cpu | head -5)
+CPUUsage=$(top -bn 1 | awk '/Cpu/ {print 100 - $8}')
+RAMUsage=$(free -h | awk '/Mem/ {print $3 "/" $2}')
+DiskUsage=$(df -h)
+Top5Processes=$(ps aux --sort=-%cpu | head -6)
 
 echo ==========================
 echo Linux Server Health Report 
@@ -16,9 +22,10 @@ echo "Hostname               : $ComputerName"
 echo "Current Logged In User : $CurrentUser"
 echo "Time                   : $CurrentDate"
 echo "Kernel Version         : $KernelName"
-echo Current CPU Usage:
-echo $CPUUsage
-echo Current RAM Usage:
-echo $RAMUsage
-echo Top 5 Processes
-echo $Top5Processes
+echo "Current CPU Usage      : $CPUUsage%"
+echo "Current RAM Usage      : $RAMUsage"
+echo "Disk Usage             : "
+echo "$DiskUsage"
+echo "Top 5 Processes        : "
+echo "$Top5Processes"
+echo ==========================
