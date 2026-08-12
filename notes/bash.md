fd1dd4d36f4481b92e6eec91ead0a1dbccf3dab3
Dharshan078 <sivadharshan980@gmail.com>
2026-08-12 11:51:45 +0000

    pythonHealthMonitor script v1.1 completed

---
+++ notes/bash.md
@@ -0,0 +1,3 @@
+use ctrl+,
+quick suggestion edit settings.json
+Command Palette (Ctrl+Shift+P) → GitHub Copilot: Disable || To disbale inline suggestion
--- projects/project-04-pythonLinuxHealthMonitor/pythonHealthMonitor.py
+++ projects/project-04-pythonLinuxHealthMonitor/pythonHealthMonitor.py
@@ -15,32 +15,63 @@ hostname = subprocess.run(
     capture_output=True,
     text=True
 )
-print("Hostname: ",hostname.stdout)
 
 username = subprocess.run(
     ["whoami"],
     capture_output=True,
     text=True
 )
-print("Current logged in Username: ", username.stdout)
 
 date = subprocess.run(
     ["date"],
     capture_output=True,
     text=True
 )
-print("Current Time: ", date.stdout)
 
 kernelversion = subprocess.run(
     ["uname", "-r"],
     capture_output=True,
     text=True
 )
-print("Kernel Version: ", kernelversion.stdout)
 
 cpuUsage = subprocess.run(
-    ["top","-bn","1","|","awk","'/Cpu/ {print 100 - $8}'"],
+    "top -bn1 | grep 'Cpu' | awk '{print 100 - $8}'",
+    shell=True,
     capture_output=True,
     text=True
 )
-print("CPU Usage: ",cpuUsage.stdout)
+
+ramUsage = subprocess.run(
+    "free -h | grep 'Mem' | awk '{print $3,$2}'",
+    shell=True,
+    capture_output=True,
+    text=True,
+)
+ramOutput = ramUsage.stdout.strip()
+ramValues = ramOutput.split()
+ramUsed = ramValues[0]
+ramTotal = ramValues[1]
+
+diskUsage = subprocess.run(
+    "df -h",
+    shell=True,
+    capture_output=True,
+    text=True
+)
+
+Top5Process = subprocess.run(
+    "ps aux --sort=-%cpu | head -6",
+    shell=True,
+    capture_output=True,
+    text=True
+)
+
+
+print("Hostname: ",hostname.stdout)
+print("Current logged in Username: ", username.stdout)
+print("Current Time: ", date.stdout)
+print("Kernel Version: ", kernelversion.stdout)
+print(f"Cpu Usage: {cpuUsage.stdout.strip()}%")
+print(f"Ram Utilization: {ramUsed}\nRam Overall: {ramTotal}")
+print(f"Disk Usage: \n{diskUsage.stdout}")
+print(f"Top 5 Process \n {Top5Process.stdout}")
