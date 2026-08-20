788ad13e676b49d8715fdd581f387af74fd4883d
Dharshan078 <sivadharshan980@gmail.com>
2026-08-20 11:59:49 +0000

    Python Healthmonitor psutil function System, CPU, Memory info completed

--- projects/project-04-pythonLinuxHealthMonitor/python_healthmonitor_psutil_function.py
+++ projects/project-04-pythonLinuxHealthMonitor/python_healthmonitor_psutil_function.py
@@ -9,21 +9,57 @@ import time
 import datetime
 import socket
 
-def sectotime(x):
-    x = ((x%86400) / 3600)
-    return x
-
 def main():
     system = get_system_info()
-    return system
+    cpu = get_cpu_info()
+    memory = get_mem_info()
+    return system, cpu, memory
+
+def bytes_to_gb(value):
+    return round(value / (1024 ** 3), 2)
+def bytes_to_mb(value):
+    return round(value / (1024 ** 2), 2)
+
+print("="*60)
+print("                  LINUX SERVER HEALTH REPORT")
+print("="*60)
 
 def get_system_info():
-    hostname = print(f"Hostname:    {socket.gethostname()}")
-    boottime = print(f"Boottime:    {psutil.boot_time()}")
-    return hostname, sectotime(boottime)
+    print("\nSystem")
+    print("------")
+    hostname = socket.gethostname()
+    boottime = datetime.datetime.fromtimestamp(psutil.boot_time())
+    uptime = datetime.datetime.fromtimestamp(time.time() - psutil.boot_time())
+    print(f"Hostname:   {hostname}")
+    print(f"Boottime:   {boottime.strftime("%H:%M:%S")}")
+    print(f"Uptime: {uptime.strftime("%H:%M:%S")}")
+    return hostname, boottime, uptime
 
 def get_cpu_info():
+    print("\nCPU")
+    print("---")
+    cpu_usage = psutil.cpu_percent(interval=0.1)
+    cpu_cores = psutil.cpu_count(logical=False)
+    logical_cores = psutil.cpu_count(logical=True)
+    per_cpu = psutil.cpu_percent(interval=0.1, percpu=True)
+    print(f"CPU Usage:  {cpu_usage}%")
+    print(f"Physical Cores: {cpu_cores}")
+    print(f"Logical Cores:  {logical_cores}")
+    print(f"Per CPU Usage:  {per_cpu}")
+    return cpu_usage, cpu_cores, logical_cores, per_cpu
 
-    return
+def get_mem_info():
+    print("\nMemory")
+    print("------")
+    memory = psutil.virtual_memory()
+    total = memory.total
+    usage = memory.used
+    available = memory.available
+    mem_percent = memory.percent
+    print(f"Total Memory:   {bytes_to_gb(total)}GB")
+    print(f"Used Memory:    {bytes_to_mb(usage)}GB")
+    print(f"Available Memory:   {bytes_to_mb(available)}MB")
+    print(f"Memory Used Percent:    {mem_percent}%")
+    return memory, total, usage, available, mem_percent
 
 main()
