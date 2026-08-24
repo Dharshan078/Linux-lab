55a9081195f0705d550580122c90087123ee1316
Dharshan078 <sivadharshan980@gmail.com>
2026-08-24 04:22:16 +0000

    Python Health monitor functions updated to use return and print from main function

--- projects/project-04-pythonLinuxHealthMonitor/python_healthmonitor_psutil_function.py
+++ projects/project-04-pythonLinuxHealthMonitor/python_healthmonitor_psutil_function.py
@@ -1,7 +1,7 @@
 #########################################################
 # Linux Server Health Report Script with psutil function
 # Author: Sivadharshan
-# Version: 2.2
+# Version: 2.3
 #########################################################
 
 import psutil
@@ -10,94 +10,60 @@ import datetime
 import socket
 
 def main():
+    print("="*60)
+    print("                  LINUX SERVER HEALTH REPORT")
+    print("="*60)
+
     system = get_system_info()
     cpu = get_cpu_info()
     memory = get_mem_info()
-    disk = get_disk_info()
-    partition = get_partition_info()
-    netaddress = get_net_address()
+    disks = get_disk_info()
+    partitions = get_partition_info()
+    netaddress = get_net_addr()
+    netinfo = get_net_info()
     top_process = get_top_process()
-    return system, cpu, memory, disk, partition, netaddress, top_process
 
-def bytes_to_gb(value):
-    return round(value / (1024 ** 3), 2)
-def bytes_to_mb(value):
-    return round(value / (1024 ** 2), 2)
-
-print("="*60)
-print("                  LINUX SERVER HEALTH REPORT")
-print("="*60)
-
-def get_system_info():
     print("\nSystem")
     print("------")
-    hostname = socket.gethostname()
-    boottime = datetime.datetime.fromtimestamp(psutil.boot_time())
-    uptime_seconds = time.time() - psutil.boot_time()
-    uptime = datetime.timedelta(seconds=uptime_seconds)
-    print(f"Hostname:   {hostname}")
-    print(f"Boottime:   {boottime.strftime("%H:%M:%S")}")
-    print(f"Uptime: {uptime}")
-    return hostname, boottime, uptime
+    print(f"Hostname:   {system[0]}")
+    print(f"Boottime:   {system[1].strftime("%H:%M")}")
+    print(f"Uptime: {system[2]}")
 
-def get_cpu_info():
     print("\nCPU")
     print("---")
-    cpu_usage = psutil.cpu_percent(interval=0.1)
-    cpu_cores = psutil.cpu_count(logical=False)
-    logical_cores = psutil.cpu_count(logical=True)
-    per_cpu = psutil.cpu_percent(interval=0.1, percpu=True)
-    print(f"CPU Usage:  {cpu_usage}%")
-    print(f"Physical Cores: {cpu_cores}")
-    print(f"Logical Cores:  {logical_cores}")
-    print(f"Per CPU Usage:  {per_cpu}")
-    return cpu_usage, cpu_cores, logical_cores, per_cpu
+    print(f"CPU Usage       :{cpu[0]}%")
+    print(f"Physical Cores  :{cpu[1]}")
+    print(f"Logical Cores   :{cpu[2]}")
+    print(f"Per CPU Usage   :{cpu[3]}")
 
-def get_mem_info():
     print("\nMemory")
     print("------")
-    memory = psutil.virtual_memory()
-    total = memory.total
-    usage = memory.used
-    available = memory.available
-    mem_percent = memory.percent
-    print(f"Total Memory:   {bytes_to_gb(total)}GB")
-    print(f"Used Memory:    {bytes_to_mb(usage)}GB")
-    print(f"Available Memory:   {bytes_to_mb(available)}MB")
-    print(f"Memory Used Percent:    {mem_percent}%")
-    return memory, total, usage, available, mem_percent
+    print(f"Total Memory: {bytes_to_gb(memory.total)} GB")
+    print(f"Used Memory: {bytes_to_mb(memory.used)} MB")
+    print(f"Available Memory: {bytes_to_mb(memory.available)} MB")
+    print(f"Memory Used Percent: {memory.percent}%")
 
-def get_disk_info():
     print("\nDisk")
     print("----")
-    disk = psutil.disk_usage("/")
-    disktotal = disk.total
-    diskused = disk.used
-    diskfree = disk.free
-    diskpercent = disk.percent
+    disktotal = disks.total
+    diskused = disks.used
+    diskfree = disks.free
+    diskpercent = disks.percent
     print(f"Total Disk: {bytes_to_gb(disktotal)}GB")
     print(f"Total Disk Usage: {bytes_to_gb(diskused)}GB")
     print(f"Total Available Space: {bytes_to_mb(diskfree)}MB")
     print(f"Disk Used Percent: {diskpercent}%")
-    return disk
 
-def get_partition_info():
     print("\nPartitions")
     print("----------")
-    partitions = []
-    for partition in psutil.disk_partitions():
-        partitions.append(partition)
-    for parition in partitions:
-        print(f"Parition Device:    ", parition.device)
-        print(f"Mount Point:    ", parition.mountpoint)
+    for partition in partitions:
+        print(f"Parition Device:    ", partition.device)
+        print(f"Mount Point:    ", partition.mountpoint)
         print(f"FileSystem:     ", partition.fstype)
-    return partitions
 
-def get_net_info():
     print("\nNetwork")
     print("-------")
-    net = psutil.net_io_counters(pernic=True)
-    for interface, stats in net.items():
+    for interface, stats in netinfo.items():
         print(interface)
         print(" Sent                     :", bytes_to_mb(stats.bytes_sent),"MB")
         print(" Received                 :", bytes_to_mb(stats.bytes_recv),"MB")
@@ -105,24 +71,65 @@ def get_net_info():
         print(f" Packets Receieved       : {stats.packets_recv}")
         print(f" Drop                    : IN = {stats.dropin} | OUT = {stats.dropout}")
         print(f" Error                   : IN = {stats.errin} | OUT = {stats.errout}")
-    return net
 
-def get_net_addr():
     print("\nNetwork Adresses")
     print("---------------")
-    
-    return
+    for interface, addresses in netaddress.items():
+        print("Interface: ", interface)
+        for address in addresses:
+            print("Address: ", address.address)
 
-def get_top_process():
     print("\nTop Process")
     print("-----------")
+    for process in top_process:
+        print(process)
+
+    return system, cpu, memory, disks, partition, netaddress, top_process, netinfo
+
+def bytes_to_gb(value):
+    return round(value / (1024 ** 3), 2)
+def bytes_to_mb(value):
+    return round(value / (1024 ** 2), 2)
+
+
+def get_system_info():
+    hostname = socket.gethostname()
+    boottime = datetime.datetime.fromtimestamp(psutil.boot_time())
+    uptime_seconds = time.time() - psutil.boot_time()
+    uptime = datetime.timedelta(seconds=uptime_seconds)
+    return hostname, boottime, uptime
+
+def get_cpu_info():
+    cpu_usage = psutil.cpu_percent(interval=0.1)
+    cpu_cores = psutil.cpu_count(logical=False)
+    logical_cores = psutil.cpu_count(logical=True)
+    per_cpu = psutil.cpu_percent(interval=0.1, percpu=True)
+    return cpu_usage, cpu_cores, logical_cores, per_cpu
+
+def get_mem_info():
+    return psutil.virtual_memory()
+
+def get_disk_info():
+    return psutil.disk_usage("/")
+
+def get_partition_info():
+    partitions = []
+    for partition in psutil.disk_partitions():
+        partitions.append(partition)
+    return partitions
+
+def get_net_info():
+    return psutil.net_io_counters(pernic=True) 
+
+def get_net_addr():
+    return psutil.net_if_addrs()
+
+def get_top_process():
     processes=[]
     for process in psutil.process_iter(["pid", "name", "cpu_percent", "memory_percent"]):
         processes.append(process.info)
     processes.sort(key=lambda x: x['cpu_percent'], reverse=True)
-    for process in processes[:5]:
-        print(process)
     top_processes = processes[:5]
     return top_processes
 
-main()
+result = main()
