e272da94ebcb186a1602edce3156a753e5703d3d
Dharshan078 <sivadharshan980@gmail.com>
2026-08-31 12:03:16 +0000

    Identity check complete ec2 inventory started

--- projects/aws/project-07-boto3/identity_check.py
+++ projects/aws/project-07-boto3/identity_check.py
@@ -1,5 +1,16 @@
 import boto3
-sts = boto3.client("sts")
-response = sts.get_caller_identity()
-print(sts)
-print(response["UserId"])
+
+def main():
+    identity = get_identity()
+    print("User ID  :",identity["UserId"])
+    print("Account  :",identity["Account"])
+    print("ARN      :",identity["Arn"])
+    return identity
+
+def get_identity():
+    sts = boto3.client("sts")
+    response = sts.get_caller_identity()
+    return response
+
+if __name__ == "__main__":
+    main()
