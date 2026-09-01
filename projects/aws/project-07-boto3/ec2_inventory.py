cb527edb751f09cf38532418331c19a7ca7ca512
Dharshan078 <sivadharshan980@gmail.com>
2026-09-01 11:58:10 +0000

    Completed EC2 inventory need to use filters

--- projects/aws/project-07-boto3/ec2_inventory.py
+++ projects/aws/project-07-boto3/ec2_inventory.py
@@ -1,9 +1,45 @@
+#########################################################
+# AWS EC2 Inventory
+# Author: Sivadharshan
+# Version: 1.3
+# 1.0: Raw code just to print the instance information
+# 1.1: Added functions, Clean code
+# 1.2: Added try and exceptions [Clinet and BotoCore Error]
+# 1.3: Using filter
+#########################################################
+
+
 import boto3
+from botocore.exceptions import ClientError, BotoCoreError
 
-ec2 = boto3.client("ec2")
+def main():
+    print("="*40)
+    print("EC2 INSTANCE INVENTORY")
+    print("="*40)
+    get_instance = get_instances()
+    if not get_instance:
+        print("No EC2 instances found")
+        return
+    else:
+         for instance in get_instance:
+            print("Reservation ID   :",instance["ReservationId"])
+            for instances in instance["Instances"]:
+                print("Instance ID      :",instances["InstanceId"])
+                print("Instance Type    :",instances["InstanceType"])
+                print("Instance State   :",instances["State"]["Name"])
+                print("Private IP       :",instances.get("PrivateIpAddress","N/A"))
+                print("Availability Zone:",instances["Placement"]["AvailabilityZone"])
+          
+def get_instances():
+    try:
+        ec2 = boto3.client("ec2")
+        response = ec2.describe_instances()
+        return response["Reservations"]
+    except ClientError as error:
+        print(f"AWS API error: {error}")
+        return []
+    except BotoCoreError as error:
+        print(f"Boto3 error: {error}")
 
-response = ec2.describe_instances()
-
-for instance in response:
-    instances = response[instance]
-    print(instances)
+if __name__ == "__main__":
+     main()
